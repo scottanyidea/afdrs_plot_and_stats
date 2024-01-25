2aed5814a280f9d9256afd7168259c01f49a56a8
Scott Clark <clark@cfa.vic.gov.au>
2024-01-25 15:28:33 +1100

    Working version that runs through a single region

--- datacube_region_designate_rating.py
+++ datacube_region_designate_rating.py
@@ -11,11 +11,13 @@ from datetime import datetime
 import time
 
 
-def calc_region_rating(data_path, date_list, area_name):
+def calc_region_rating(data_path, date_list, area_shape_path, area_name, fuel_lut_path):
+    from replace_fueltype_changes_designate_rating import find_dominant_fuel_type_for_a_rating
     dates_used = []
     fbi_list = []
     rating_list = []
     fdi_list = []
+    dom_type = []
     for dt in date_list:
         time_start = time.time()
         print('starting '+str(dt))
@@ -28,7 +30,7 @@ def calc_region_rating(data_path, date_list, area_name):
             recalc_max = file_in['index_1'].max(dim='time',skipna = True, keep_attrs=True)
         
             #Filter down to desired FWA. #EPSG:4326 corresponds to WGS 1984 CRS
-            shp_in = geopandas.read_file("C://Users/clark/analysis1/afdrs_fbi_recalc-main/data/shp/PID90109_VIC_Boundary_SHP_FWA\PID90109_VIC_Boundary_SHP_FWA.shp", crs='ESPG:4326')
+            shp_in = geopandas.read_file(area_shape_path, crs='ESPG:4326')
             area_polygon = shp_in[shp_in['Area_Name']==area_name]
             recalc_max.rio.set_spatial_dims(x_dim='longitude',y_dim='latitude',inplace=True) #OK doing this makes a lot more sense now there's a time dimension. It's telling rioxarray what the spatial dims are!
             recalc_max.rio.write_crs("EPSG:4326",inplace=True)  #And now tell it the coord reference system.
@@ -40,6 +42,11 @@ def calc_region_rating(data_path, date_list, area_name):
             fbi_list.append(desig_fbi)
             rating_list.append(desig_rating)
             
+            #Find dominant model for each rating (ie. what's driven the 90th percentile)
+            fuel_map = file_in['fuel_type']
+            dom_typ_str = find_dominant_fuel_type_for_a_rating(clipped_recalc, desig_fbi, fuel_map, fuel_lut_path)
+            dom_type.append(dom_typ_str)
+
             #If we get to this point, grab the date.
             dates_used.append(dt)
                         
@@ -59,7 +66,7 @@ def calc_region_rating(data_path, date_list, area_name):
             time_end = time.time()
             print('Time for this iteration: '+str(time_end - time_start))
     
-    rating_table = pd.DataFrame(list(zip(dates_used, fbi_list, rating_list, fdi_list)), columns=['Date', 'FBI','Rating', 'McArthur_FDI'])
+    rating_table = pd.DataFrame(list(zip(dates_used, fbi_list, rating_list, dom_type, fdi_list)), columns=['Date', 'FBI','Rating', 'Dominant FT', 'McArthur_FDI'])
 
     return rating_table
  
@@ -81,11 +88,17 @@ def rating_calc(fbi):
 
 if __name__=="__main__":
     fbi_data_path = 'C:/Users/clark/analysis1/afdrs_fbi_recalc-main/Recalculated_VIC_Grids/full_recalc_jan_24/recalc_files/'
+#    fbi_data_path = 'C:/Users/clark/analysis1/afdrs_fbi_recalc-main/Recalculated_VIC_Grids/v2024.1b7/full_recalc_jan_24/recalc_files/'
+     
+    fuel_lut_path = "C:/Users/clark/analysis1/afdrs_fbi_recalc-main/data/fuel/fuel-type-model-authorised-vic-20231012043244.csv"
+    
+#    shp_path = "C://Users/clark/analysis1/afdrs_fbi_recalc-main/data/shp/PID90109_VIC_Boundary_SHP_FWA\PID90109_VIC_Boundary_SHP_FWA.shp"
+    shp_path = "C://Users/clark/analysis1/afdrs_fbi_recalc-main/data/shp/PID90409_VIC_Boundary_SHP_LGA\PID90409_VIC_Boundary_SHP_LGA.shp"
     
     #Set dates:
-    dates_ = pd.date_range(datetime(2017,10,1), datetime(2022,5,31), freq='D')
-        
-    area_name = 'North East'
+#    dates_ = pd.date_range(datetime(2017,10,1), datetime(2022,5,31), freq='D')
+    dates_ = pd.date_range(datetime(2020,10,16), datetime(2020,10,17), freq='D')        
+    area_name = 'Latrobe'
 
-    fbi_and_rating = calc_region_rating(fbi_data_path, dates_, area_name)
-    fbi_and_rating.to_csv("C:/Users/clark/analysis1/datacube_daily_stats/"+area_name+"_datacube_20172022_fbi_rating.csv")
+    fbi_and_rating = calc_region_rating(fbi_data_path, dates_, shp_path, area_name, fuel_lut_path)
+#    fbi_and_rating.to_csv("C:/Users/clark/analysis1/datacube_daily_stats/version_feb24/"+area_name+"_datacube_2017-2022_fbi_rating.csv")
