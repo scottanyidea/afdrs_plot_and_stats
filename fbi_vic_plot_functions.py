481f20498da90008be9cf14827e1001855bb92c4
Scott Clark <scott.clark@cfa.vic.gov.au>
2024-03-01 09:14:44 +1100

    Some plotting functions added

--- fbi_vic_plot_functions.py
+++ fbi_vic_plot_functions.py
@@ -60,7 +60,7 @@ def plot_and_compare_fbi(max_fbi1, max_fbi2, areas_shapefile, save_plot=None):
         plt.savefig(save_plot+'.png')
 
     
-def plot_fbi_and_rating_with_fwas(FBI,rating,areas_shapefile, save_plot=None):
+def plot_fbi_and_rating_with_fwas(FBI,rating,areas_shapefile, box_extent=None, save_plot=None):
     """
     Plots FBI and fire danger rating from xarray DataArray inputs. 
 
@@ -77,11 +77,17 @@ def plot_fbi_and_rating_with_fwas(FBI,rating,areas_shapefile, save_plot=None):
     None. Saves plot as a PNG with the name given by save_plot.
 
     """
+    if box_extent is None:
+        box_extent = [140.8,150,-39.3,-33.8]
     
     fig, axs = plt.subplots(1,2,figsize=(12,6), subplot_kw={'projection': ccrs.PlateCarree()})
-    cmap_rating = pltcolors.ListedColormap(['white','green','gold','darkorange','red'])
+    
+    cmap_rating = pltcolors.ListedColormap(['blue','green','gold','darkorange','red'])
     norm = pltcolors.BoundaryNorm([0,1,2,3,4,5], cmap_rating.N)
-
+    """
+    cmap_rating = pltcolors.ListedColormap(['blue','green'])
+    norm = pltcolors.BoundaryNorm([0,1,2], cmap_rating.N)
+    """
     im1 = FBI.plot(ax=axs[0], transform=ccrs.PlateCarree(),vmin=0., vmax=50., cmap='viridis', add_colorbar=False)
     cb1 = plt.colorbar(im1, orientation='vertical')
     cb1.set_label(label='FBI',size=14)
@@ -97,15 +103,15 @@ def plot_fbi_and_rating_with_fwas(FBI,rating,areas_shapefile, save_plot=None):
     axs[0].set_xticks([142,144,146,148,150], crs=ccrs.PlateCarree())
     axs[0].set_yticks([-38,-36,-34], crs=ccrs.PlateCarree())
 #    axs[0].set_extent([140.8,147.8,-38.4,-33.8])
-    axs[0].set_extent([140.8,144.7,-37.3,-34.2])
-#    axs[0].set_extent([142,144,-36.3,-34])
+    axs[0].set_extent(box_extent)
+#    axs[0].set_extent([143,146.2,-37.3,-34.5])  #Northern Country
     axs[1].coastlines()
     axs[1].set_title('Rating',fontsize=16)
     axs[1].set_xticks([142,144,146,148,150], crs=ccrs.PlateCarree())
     #axs[1].set_yticks([-38,-36,-34], crs=ccrs.PlateCarree())
 #    axs[1].set_extent([140.8,147.8,-38.4,-33.8])
-    axs[1].set_extent([140.8,144.7,-37.3,-34.2])
-#    axs[1].set_extent([142,144,-36.3,-34])        
+    axs[1].set_extent(box_extent)
+#    axs[1].set_extent([143,146.2,-37.3,-34.5])         #Northern Country
 
     if save_plot is not None:
         plt.savefig(save_plot+'.png')
@@ -187,3 +193,88 @@ def scatterplot_fbi_vs_fdi_months(fdi, fbi, month_list, title_str=None, out_file
 
     if out_file_path is not None:
         plt.savefig(out_file_path)
+
+def plot_df(df, areas_shapefile, save_plot=None):
+    """
+    Plot drought factor on a single plot.
+
+    Parameters
+    ----------
+    df (xarray DataArray) : Drought factor array, must be 2-dimensional with lat and lon coordinate variables.
+    areas_shapefile (geopandas object): Shapefile that contains sub-regions that will be plotted as outlines overlaying
+    the FBI data.
+    save_plot (string, optional) : Name to save the plot. 
+
+    Returns
+    -------
+    No output arguments. Option to save plot as a PNG with the name given by save_plot.
+
+    """
+    
+    fig, axs = plt.subplots(1,figsize=(8,8), subplot_kw={'projection': ccrs.PlateCarree()})
+    cmap_df = pltcolors.ListedColormap(['mediumblue','blue','royalblue','cornflowerblue','darkturquoise','aquamarine','springgreen','gold','darkorange','red','darkred'])
+    norm = pltcolors.BoundaryNorm([0,1,2,3,4,5,6,7,8,9,10], cmap_df.N)
+    
+    im1 = df.plot(ax=axs, transform=ccrs.PlateCarree(), cmap=cmap_df, norm=norm, add_colorbar=False)
+    cb1 = plt.colorbar(im1, orientation='vertical')
+    cb1.set_label('Drought factor', size=16)
+    cb1.ax.tick_params(labelsize=16)
+    areas_shapefile.plot(ax=axs, facecolor="none")
+
+    axs.coastlines()
+    axs.set_title('Drought factor', fontsize=20)
+    axs.set_xticks([142,144,146,148,150], crs=ccrs.PlateCarree())
+    axs.set_yticks([-38,-36,-34], crs=ccrs.PlateCarree())
+#    axs[0].set_extent([140.8,144.7,-37.3,-34.2])
+    axs.set_extent([140.8,145.7,-39,-33.8])   #Wimmera + SW
+ 
+    axs.set_xlabel('')
+    axs.set_ylabel('')
+    
+    
+    if save_plot is not None:
+        plt.savefig(save_plot+'.png')
+
+def plot_fbi_and_fdi(fbi, fdi, areas_shapefile, save_plot=None):
+    fig, axs = plt.subplots(1,2,figsize=(12,6), subplot_kw={'projection': ccrs.PlateCarree()})
+
+    fbi.plot(ax=axs[0], transform=ccrs.PlateCarree(),vmin=0., vmax=100., cmap='viridis')
+    areas_shapefile.plot(ax=axs[0], facecolor="none")
+    fdi.plot(ax=axs[1], transform=ccrs.PlateCarree(), vmin=0., vmax=100., cmap='viridis')
+    areas_shapefile.plot(ax=axs[1], facecolor="none")
+    axs[0].coastlines()
+    axs[0].set_title('FBI')
+    axs[0].set_xticks([142,144,146,148,150], crs=ccrs.PlateCarree())
+    axs[0].set_yticks([-38,-36,-34], crs=ccrs.PlateCarree())
+#    axs[0].set_extent([140.8,144.7,-37.3,-34.2])
+#    axs[0].set_extent([142.8,145.7,-38.8,-37.0])
+    axs[0].set_extent([140.8,145.7,-39,-33.8])   #Wimmera + SW
+    axs[1].coastlines()
+    axs[1].set_title('FDI')
+    axs[1].set_xticks([142,144,146,148,150], crs=ccrs.PlateCarree())
+    axs[1].set_yticks([-38,-36,-34], crs=ccrs.PlateCarree())
+    #axs[1].set_extent([142.8,145.7,-38.8,-37.0])
+    axs[1].set_extent([140.8,145.7,-39,-33.8])   #Wimmera + SW
+#    axs[1].set_extent([140.8,144.7,-37.3,-34.2])
+
+    
+    if save_plot is not None:
+        plt.savefig(save_plot+'.png')
+        
+
+
+def fbi_rating_calc_spatial(fbi):
+    if fbi < 12:
+        rating = 0
+    else:
+        if fbi < 24:
+            rating = 1
+        else:
+            if fbi < 50:
+                rating = 2
+            else:
+                if fbi <100:
+                    rating = 3
+                else:
+                    rating = 4
+    return rating
